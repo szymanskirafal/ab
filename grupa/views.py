140772806d7363e5677957fcd6eb083f629f33ff
RafalSzymanski <r.szymansky@gmail.com>
2015-12-15 14:03:38 +0000

    looking trough certain group

--- grupa/views.py
+++ grupa/views.py
@@ -14,21 +14,33 @@ from .forms import NewGroupForm
 
 @login_required
 def grupa(request):
-    return render(request, 'grupa/grupa.html')
+    current_user = request.user
+    grupy = current_user.groups.all()
+    return render(request, 'grupa/grupa.html', {
+        'current_user': current_user,
+        'grupy': grupy})
 
 
 
 
 @login_required
 def nowa(request):
+    group_creator = request.user
     if request.method == 'POST':
         form = NewGroupForm(request.POST)
         if form.is_valid():
             group_name = form.cleaned_data['group_name']
             new_group = Group.objects.create(name=group_name)
+            new_group.user_set.add(group_creator)
 
             return HttpResponseRedirect('/dodane/')
     else:
         form = NewGroupForm()
 
     return render(request, 'grupa/new_group.html', {'form': form})
+
+
+@login_required
+def group(request, group_name):
+
+    return render(request, 'grupa/group.html')
